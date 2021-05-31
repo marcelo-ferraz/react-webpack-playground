78f31a035be95b0b3acc22a6c70a6b3830e77a25
marcelo-ferraz <marcelosfm@gmail.com>
2021-06-01 08:51:42 +1200

    forcing the rename

--- src/Components/Playground/index.js
+++ src/Components/Playground/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer, useRef } from 'react';
 import Editor from '../Editor';
-import Display from '../display';
+import Display from '../Displays';
 import contextReducer from '../../contextReducer';
 
 import { defaultEntryPath } from '../../parser/webpackInvoker';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Playground from './Components/Playground';
 import contextReducer from './contextReducer';
 import Editor from './Components/Editor';
-import Display from './Components/display';
+import Display from './Components/Displays';
 
 import './simple-grid.scss';
 
