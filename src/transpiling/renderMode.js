7aaba29ff5636f2200e3c1e6861f7165d4fb5466
marcelo-ferraz <marcelosfm@gmail.com>
2021-06-24 15:49:57 +1200

    adding a modes for the renderer

---
+++ src/transpiling/renderMode.js
@@ -0,0 +1,6 @@
+const renderMode = {
+    parallel: 0,
+    inline: 1,
+};
+
+export default renderMode;
--- src/transpiling/use4DynamicEsModules.js
+++ src/transpiling/use4DynamicEsModules.js
@@ -1,48 +1,57 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 import { pathFinder } from './helpers';
 
 import stage from './stage';
-import { renderElsewhere as webpackRender, jsInvoke, defaultEntryPath } from './webpackInvoker';
+import renderMode from './renderMode';
+import { renderElsewhere, renderHere, jsInvoke, defaultEntryPath } from './webpackInvoker';
 
-export default function use4DynamicEsModules(context, defaultEntry) {
+export default function use4DynamicEsModules(context, defaultEntry, mode = renderMode.parallel) {
     const [status, setStatus] = useState(stage.none);
     const [error, setError] = useState();
     const invokedComponent = useRef();
 
-    const render = async (project, entryPath = defaultEntryPath) => {
-        const finder = pathFinder(project.entries);
-        const [, entry] = finder(entryPath);
+    const webpackRender = useMemo(
+        () => (mode === renderMode.parallel ? renderElsewhere : renderHere),
+        [mode],
+    );
 
-        if (!entry) {
-            return;
-        }
+    const render = useCallback(
+        async (project, entryPath = defaultEntryPath) => {
+            const finder = pathFinder(project.entries);
+            const [, entry] = finder(entryPath);
 
-        let phase = stage.rendering;
-        try {
-            setStatus(phase);
-            const ctx = await webpackRender(project, entryPath, finder);
-            if (!ctx) {
-                setStatus(stage.notInvoked | stage.finished);
-                invokedComponent.current = null;
+            if (!entry) {
                 return;
             }
-            setStatus((phase = stage.invoking));
 
-            const { exports } = jsInvoke(ctx);
+            let phase = stage.rendering;
+            try {
+                setStatus(phase);
+                const ctx = await webpackRender(project, entryPath, finder);
+                if (!ctx) {
+                    setStatus(stage.notInvoked | stage.finished);
+                    invokedComponent.current = null;
+                    return;
+                }
+                setStatus((phase = stage.invoking));
 
-            invokedComponent.current = exports.default;
-            setStatus(stage.invoked | stage.finished);
-        } catch (err) {
-            setError(err);
-            setStatus(phase | stage.error);
-        }
-    };
+                const { exports } = jsInvoke(ctx);
+
+                invokedComponent.current = exports.default;
+                setStatus(stage.invoked | stage.finished);
+            } catch (err) {
+                setError(err);
+                setStatus(phase | stage.error);
+            }
+        },
+        [webpackRender],
+    );
 
     useEffect(() => {
         if (context) {
             render(context, defaultEntry);
         }
-    }, [context, defaultEntry]);
+    }, [context, defaultEntry, render]);
 
     const forceRefresh = () => {
         if (!stage.has(stage.rendering, status)) {
