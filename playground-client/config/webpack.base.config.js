3c521cf39f6bcffbcb5b6a682a6f9f8bc43c6d1d
marcelo-ferraz <marcelosfm@gmail.com>
2021-08-09 22:45:13 +1200

    refactoring the build and configuration

--- playground-client/config/webpack.base.config.js
+++ playground-client/config/webpack.base.config.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const path = require('path');
+const appPackageJson = require('../package.json');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const jsRule = {
     test: /\.jsx?$/i,
@@ -15,7 +17,7 @@ const jsRule = {
 const cssRule = {
     test: /\.s?css$/i,
     exclude: [/node_modules/, /\.module\.s?css$/i],
-    use: ['style-loader', 'css-loader', 'sass-loader'],
+    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 };
 
 const cssModuleRule = {
@@ -23,7 +25,7 @@ const cssModuleRule = {
     include: /\.module\.s?css$/i,
     exclude: /node_modules/,
     use: [
-        'style-loader',
+        MiniCssExtractPlugin.loader,
         {
             loader: 'css-loader',
             options: {
@@ -40,38 +42,102 @@ const fontsRule = {
     test: /\.(svg|woff|woff2|ttf|eot)(\?[\s\S]+)?$/,
     // loader: 'file-loader?name=./fonts/[name]-[hash].[ext]',
     loader: 'file-loader',
-    options: { name: './fonts/[name]-[hash].[ext]' },
+    options: { name: 'fonts/[name]-[hash].[ext]' },
 };
 
 const htmlRule = {
     test: /\.html$/i,
     loader: 'html-loader',
-    options: { minimize: true },
+    options: {
+        minimize: true,
+    },
 };
 
 const fileRule = {
     test: /\.(png|jpg|jpeg|gif|ico|svg|webp)$/,
     loader: 'file-loader',
+    options: { name: 'static/media/[name]-[hash].[ext]' },
 };
 
-module.exports = {
-    module: {
-        rules: [jsRule, cssModuleRule, cssRule, fontsRule, htmlRule, fileRule],
+const getOutput = (isDev) => ({
+    // The build folder.
+    path: path.resolve(process.cwd(), 'dist'), // !isDev ? '' : undefined,
+    // Add /* filename */ comments to generated require()s in the output.
+    pathinfo: isDev,
+    // There will be one main bundle, and one file per asynchronous chunk.
+    // In development, it does not produce real files.
+    filename: !isDev ? 'static/js/[name].[contenthash:8].js' : 'static/js/[name].js',
+    // There are also additional JS chunk files if you use code splitting.
+    chunkFilename: (pathData) => {
+        if (pathData.chunk.id) {
+            if (
+                pathData.chunk.id.indexOf('path-browserify') >= 0 &&
+                pathData.chunk.id.indexOf('babel') >= 0
+            ) {
+                return 'babel-path-[hash].js';
+            } else if (pathData.chunk.id.indexOf('babel') >= 0) {
+                return 'babel-[hash].js';
+            }
+        }
+
+        return !isDev ? 'static/js/[name].[contenthash:8].chunk.js' : 'static/js/[name].chunk.js';
     },
-    resolve: {
-        fallback: {
-            path: require.resolve('path-browserify'),
+    // webpack uses `publicPath` to determine where the app is being served from.
+    // It requires a trailing slash, or the file assets will get an incorrect path.
+    // We inferred the "public path" (such as / or /my-project) from homepage.
+    //  publicPath: paths.publicUrlOrPath,
+    // Point sourcemap entries to original disk location (format as URL on Windows)
+    devtoolModuleFilenameTemplate: !isDev
+        ? (info) => path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, '/')
+        : (info) => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+    // Prevents conflicts when multiple webpack runtimes (from different apps)
+    // are used on the same page.
+    // jsonpFunction: `webpackJsonp${appPackageJson.name}`,
+    // this defaults to 'window', but by setting it to 'this' then
+    // module chunks which are built will work in web workers as well.
+    globalObject: 'this',
+});
+
+module.exports = (_, argv) => {
+    const isDev = argv.mode === 'development';
+
+    const output = getOutput(isDev);
+
+    return {
+        output,
+        module: {
+            rules: [jsRule, cssModuleRule, cssRule, fontsRule, htmlRule, fileRule],
         },
-    },
-    experiments: {
-        asyncWebAssembly: true,
-    },
-    plugins: [
-        // new BundleAnalyzerPlugin(),
-        new WasmPackPlugin({
-            forceMode: 'production',
-            outDir: path.resolve(__dirname, '../../playground-client/rs-parser'),
-            crateDirectory: path.resolve(__dirname, '../../parser'),
-        }),
-    ],
+        resolve: {
+            fallback: {
+                path: require.resolve('path-browserify'),
+            },
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+                name: !isDev
+                    ? 'static/js/[name].[contenthash:8].chunk.js'
+                    : 'static/js/[name].chunk.js',
+            },
+            runtimeChunk: {
+                name: (entrypoint) => `runtime-${entrypoint.name}`,
+            },
+        },
+        experiments: {
+            asyncWebAssembly: true,
+        },
+        plugins: [
+            // new BundleAnalyzerPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'static/css/[name].css',
+                chunkFilename: 'static/css/[id].css',
+            }),
+            new WasmPackPlugin({
+                forceMode: 'production',
+                outDir: path.resolve(__dirname, '../../playground-client/rs-parser'),
+                crateDirectory: path.resolve(__dirname, '../../parser'),
+            }),
+        ],
+    };
 };
--- playground-client/config/webpack.demo.config.js
+++ playground-client/config/webpack.demo.config.js
@@ -4,9 +4,21 @@ const baseConfig = require('./webpack.base.config');
 
 const getOutput = () => ({
     path: path.resolve(process.cwd(), 'dist'),
-    filename: 'index.js',
+    filename: 'js/[name].js',
     library: 'carma-forms',
     libraryTarget: 'umd',
+    chunkFilename: (pathData) => {
+        if (
+            pathData.chunk.id.indexOf('path-browserify') >= 0 &&
+            pathData.chunk.id.indexOf('babel') >= 0
+        ) {
+            return 'babel-path-[hash].js';
+        } else if (pathData.chunk.id.indexOf('babel') >= 0) {
+            return 'babel-[hash].js';
+        }
+
+        return pathData.chunk.name ? 'js/[name]-[hash].js' : 'js/[id]-[hash].js';
+    },
 });
 
 const htmlPlugin = new HtmlWebpackPlugin({
@@ -14,15 +26,18 @@ const htmlPlugin = new HtmlWebpackPlugin({
     fileName: './index.html',
 });
 
-module.exports = (_, argv) => ({
-    ...baseConfig,
-    devtool: argv.mode === 'development' ? 'source-map' : 'none',
-    entry: path.resolve(process.cwd(), './demo/index'),
-    output: getOutput(),
-    devServer: {
-        contentBase: path.join(process.cwd(), 'dist'),
-        hot: true,
-        open: true,
-    },
-    plugins: [...baseConfig.plugins, htmlPlugin],
-});
+module.exports = (_, argv) => {
+    const baseCfg = baseConfig(_, argv);
+    return {
+        ...baseCfg,
+        devtool: argv.mode === 'development' ? 'source-map' : 'none',
+        entry: path.resolve(process.cwd(), './demo/index'),
+        // output: getOutput(),
+        devServer: {
+            contentBase: path.join(process.cwd(), 'dist'),
+            hot: true,
+            open: true,
+        },
+        plugins: [...baseCfg.plugins, htmlPlugin],
+    };
+};
